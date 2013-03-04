de97ce449c0103c1faf41ed777866b9f1655aae4
Chris Patterson <cjp256@gmail.com>
2013-03-03 22:44:50 -0500

    Add convenient download script for sinit modules.

---
+++ build/download-sinit-modules.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir -p downloads
+cd downloads
+
+wget https://chrispatterson.net/pub/intel_acm_modules/01-20-2013/2nd-gen-i5-i7-sinit-51.zip
+echo "" > 2nd-gen-i5-i7-sinit-51.zip.done
+
+wget https://chrispatterson.net/pub/intel_acm_modules/01-20-2013/3rd-gen-i5-i7-sinit-51.zip
+echo "" > 3rd-gen-i5-i7-sinit-51.zip.done
