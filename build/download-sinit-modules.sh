7802d85463959094067638e717fec8367dde132c
Chris Patterson <cjp256@gmail.com>
2013-08-24 23:03:09 -0400

    download-sinit-modules: add 3rd gen v67 module.

--- build/download-sinit-modules.sh
+++ build/download-sinit-modules.sh
@@ -3,8 +3,11 @@
 mkdir -p downloads
 cd downloads
 
-wget https://chrispatterson.net/pub/intel_acm_modules/01-20-2013/2nd-gen-i5-i7-sinit-51.zip
+wget https://chrispatterson.net/pub/intel_acm_modules/2nd-gen-i5-i7-sinit-51.zip
 echo "" > 2nd-gen-i5-i7-sinit-51.zip.done
 
-wget https://chrispatterson.net/pub/intel_acm_modules/01-20-2013/3rd-gen-i5-i7-sinit-51.zip
+wget https://chrispatterson.net/pub/intel_acm_modules/3rd-gen-i5-i7-sinit-51.zip
 echo "" > 3rd-gen-i5-i7-sinit-51.zip.done
+
+wget https://chrispatterson.net/pub/intel_acm_modules/3rd-gen-i5-i7-sinit-67.zip
+echo "" > 3rd-gen-i5-i7-sinit-67.zip.done
